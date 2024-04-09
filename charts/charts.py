f4d266f4e23fe80005cfcf4e162d91fab04a992a
kevin <k-hr123@hotmail.com>
2024-04-08 22:52:19 -0500

    añadiendo documentacion

--- charts/charts.py
+++ charts/charts.py
@@ -4,7 +4,7 @@ def generate_pie_chart():
     labels = ['A','B','C']
     values = [200,34,122]
 
-    fig, ax = pylot.sublots()
+    fig, ax = pylot.subplots()
     ax.pie(values, labels = labels)
     pylot.savefig('pie.png')
     pylot.close()
